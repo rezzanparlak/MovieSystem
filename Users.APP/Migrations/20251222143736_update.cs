232b51b42e5aae0398c4b91394b1d0dcefe8d8f7
suedakca <akcasueda@gmail.com>
2025-12-22 17:45:27 +0300

    db changed

---
+++ Users.APP/Migrations/20251222143736_update.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.APP.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavourite",
+                table: "UserMovies");
+
+            migrationBuilder.DropColumn(
+                name: "MovieName",
+                table: "UserMovies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavourite",
+                table: "UserMovies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MovieName",
+                table: "UserMovies",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
